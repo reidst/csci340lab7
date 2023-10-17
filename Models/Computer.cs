0ad0dcbd3c644a570b6490b3c08654acee414d04
Simon <reidst@hendrix.edu>
2023-10-16 20:40:55 -0500

    updated pages

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace csci340lab7.Models;
 
@@ -7,10 +8,17 @@ public class Computer
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Manufacturer { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+    [Display(Name = "Initial Price (USD)")]
+
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal InitialPrice { get; set; }
+    [Display(Name = "Operating System")]
     public string? OperatingSystem { get; set; }
+    [Display(Name = "Processor Speed (MHz)")]
     public decimal ProcessorSpeed { get; set; }
+    [Display(Name = "Memory Size (KB)")]
     public decimal MemorySize { get; set; }
 }
